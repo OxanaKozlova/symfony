b9ff7fdc232600731cddade092cd804e9accc8b0
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-11-26 19:03:26 +0100

    Update php templates

--- src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/form_row.html.php
+++ src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/form_row.html.php
@@ -1,5 +1,4 @@
 <div>
     <?php echo $view['form']->label($form) ?>
-    <?php echo $view['form']->errors($form) ?>
     <?php echo $view['form']->widget($form) ?>
 </div>
--- src/Symfony/Bundle/FrameworkBundle/Resources/views/FormTable/form_row.html.php
+++ src/Symfony/Bundle/FrameworkBundle/Resources/views/FormTable/form_row.html.php
@@ -3,7 +3,6 @@
         <?php echo $view['form']->label($form); ?>
     </td>
     <td>
-        <?php echo $view['form']->errors($form); ?>
         <?php echo $view['form']->widget($form); ?>
     </td>
 </tr>
