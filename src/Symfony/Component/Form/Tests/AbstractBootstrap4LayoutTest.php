00ec9728e17bd5c70fa4eca39906e3106c726f50
Mathieu Piot <mathieu.piot@inra.fr>
2018-02-28 08:19:01 +0100

    Fix coding standards

--- src/Symfony/Component/Form/Tests/AbstractBootstrap4LayoutTest.php
+++ src/Symfony/Component/Form/Tests/AbstractBootstrap4LayoutTest.php
@@ -153,7 +153,7 @@ abstract class AbstractBootstrap4LayoutTest extends AbstractBootstrap3LayoutTest
     public function testHelp()
     {
         $form = $this->factory->createNamed('name', 'Symfony\Component\Form\Extension\Core\Type\TextType', null, [
-            'help' => 'Help text test!'
+            'help' => 'Help text test!',
         ]);
         $view = $form->createView();
         $html = $this->renderHelp($view);
