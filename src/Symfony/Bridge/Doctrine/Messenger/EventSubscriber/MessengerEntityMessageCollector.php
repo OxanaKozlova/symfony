8e598407db7b916beb36bcd9b3264fadddddf0f6
Nyholm <tobias.nyholm@gmail.com>
2018-10-13 14:11:40 +0200

    Add support for "recording" events from entities

---
+++ src/Symfony/Bridge/Doctrine/Messenger/EventSubscriber/MessengerEntityMessageCollector.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Bridge\Doctrine\Messenger\EventSubscriber;
+
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Events;
+use Symfony\Bridge\Doctrine\Messenger\EntityMessage\EntityMessageCollectionInterface;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Middleware\Configuration\Transaction;
+
+/**
+ * Doctrine listener that listens to Persist, Update and Remove. Every time this is
+ * invoked we take messages from the entities.
+ *
+ * @author Tobias Nyholm <tobias.nyholm@gmail.com>
+ * @author Matthias Noback <matthiasnoback@gmail.com>
+ */
+class MessengerEntityMessageCollector implements EventSubscriber
+{
+    private $collectedMessage = array();
+
+    private $messageBus;
+
+    public function __construct(MessageBusInterface $messageBus)
+    {
+        $this->messageBus = $messageBus;
+    }
+
+    public function getSubscribedEvents()
+    {
+        return array(
+            Events::postPersist,
+            Events::postUpdate,
+            Events::postRemove,
+        );
+    }
+
+    public function postPersist(LifecycleEventArgs $event)
+    {
+        $this->collectEventsFromEntity($event);
+    }
+
+    public function postUpdate(LifecycleEventArgs $event)
+    {
+        $this->collectEventsFromEntity($event);
+    }
+
+    public function postRemove(LifecycleEventArgs $event)
+    {
+        $this->collectEventsFromEntity($event);
+    }
+
+    private function collectEventsFromEntity(LifecycleEventArgs $message)
+    {
+        $entity = $message->getEntity();
+
+        if ($entity instanceof EntityMessageCollectionInterface) {
+            foreach ($entity->getRecordedMessages() as $message) {
+                $this->messageBus->dispatch((new Envelope($message))->with(new Transaction()));
+            }
+
+            $entity->resetRecordedMessages();
+        }
+    }
+}
